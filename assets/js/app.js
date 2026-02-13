4537a773ceba8a18c4c8a76b49216fa18268325c
shwethalokugamage <shwethalokugamage@gmail.com>
2026-02-13 15:39:21 +0530

    implement number guessing logic and update HTML structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,28 @@
-console.log('hi')
+let randomNum = Math.floor(Math.random() * 10) + 1;
+console.log(randomNum)
+
+function guess() {
+    let input = document.getElementById('guessnumber');
+
+    if (input.value == randomNum) {
+        console.log('win')
+        Swal.fire({
+            title: "You Won!",
+            text: "Good Job Play Again!",
+            icon: "success"
+        });
+    } else if (input.value > randomNum) {
+        Swal.fire({
+            title: "You lose!",
+            text: "Too High ,Play Again!",
+            icon: "error"
+        });
+    } else {
+        Swal.fire({
+            title: "You lose!",
+            text: "Too Low Play Again!",
+            icon: "error"
+        });
+    }
+
+}
